b050f8c7344613779c67ae16b49b40b25fe2af74
Ian Dexter D Marquez <iandexter@users.noreply.github.com>
2017-04-20 15:01:44 +0800

    Rearrange config

--- .chef_knife.rb
+++ .chef_knife.rb
@@ -1,10 +1,11 @@
-log_level                :info
-log_location             STDOUT
-node_name                'iandexter'
-client_key               '/Users/iandexter/.chef/iandexter.pem'
-validation_client_name   'chef-validator'
-validation_key           '/Users/iandexter/.chef/chef-validator.pem'
-chef_server_url          'https://chef-server:443'
-syntax_check_cache_path  '/Users/iandexter/.chef/syntax_check_cache'
-cookbook_path [ '/path/to/chef/repo/cookbooks/' ]
+chef_server_url         'https://chef.internal.compareglobal.co.uk:443'
+client_key              '/Users/iandexter/.chef/iandexter_node.pem'
+cookbook_path           [ '/Users/iandexter/projects/compareglobal/infrastructure/chef-repo/cookbooks' ]
+log_level               :info
+log_location            STDOUT
+node_name               'iandexter_node'
+ssl_verify_mode         :verify_none
+syntax_check_cache_path '/Users/iandexter/.chef/syntax_check_cache'
+validation_client_name  'chef-validator'
+validation_key          '/Users/iandexter/.chef/chef-validator.pem'
 knife[:editor] = '/usr/bin/vim'
